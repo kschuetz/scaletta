1ea39d2d7640b20f5a0a1e99fd927b058446c93b
Kevin Schuetz <schuetzk@gmail.com>
2025-12-09 22:37:02 -0600

    Reduce boilerplate in NumericLiteralsTest

--- shared/src/test/scala/software/kes/scaletta/scanner/NumericLiteralsTest.scala
+++ shared/src/test/scala/software/kes/scaletta/scanner/NumericLiteralsTest.scala
@@ -28,85 +28,84 @@ class NumericLiteralsTest extends AnyFunSpec with Matchers {
         check("000000000000000000000000000000000000000000000000000000000000000123",
           Some(success(Token.IntLiteral(123), 0, 65)))
       }
+
+      it("Int.MinValue") {
+        check("-2147483648", Some(success(Token.IntLiteral(Int.MinValue), 0, 10)))
+      }
+
+      it("Int.MinValue with leading zeroes") {
+        check("-00002147483648", Some(success(Token.IntLiteral(Int.MinValue), 0, 14)))
+      }
+
+      it("Int.MinValue-1") {
+        check("-2147483649", Some(failure(IntegerNumberTooLarge, 0, 10)))
+      }
+
+      it("Int.MaxValue") {
+        check("2147483647", Some(success(Token.IntLiteral(Int.MaxValue), 0, 9)))
+      }
+
+      it("Int.MaxValue with leading zeroes") {
+        check("00002147483647", Some(success(Token.IntLiteral(Int.MaxValue), 0, 13)))
+      }
+
+      it("Int.MaxValue+1") {
+        check("2147483648", Some(failure(IntegerNumberTooLarge, 0, 9)))
+      }
     }
 
-    it("Int.MinValue") {
-      check("-2147483648", Some(success(Token.IntLiteral(Int.MinValue), 0, 10)))
+    describe("binary") {
+      it("int (small)") {
+        check("0b1010_1010", Some(success(Token.IntLiteral(170), 0, 10)))
+      }
+
+      it("long (small)") {
+        check("0b1010_1010L", Some(success(Token.LongLiteral(170), 0, 11)))
+      }
+
+      it("large") {
+        check("0b1010_1010_1010_1010_1010_1010_1010_1010_1010L", Some(success(Token.LongLiteral(45812984490L), 0, 46)))
+      }
+
+      it("negative int (small)") {
+        check("-0b1010_1010", Some(success(Token.IntLiteral(-170), 0, 11)))
+      }
+
+      it("negative long (small)") {
+        check("-0b1010_1010L", Some(success(Token.LongLiteral(-170), 0, 12)))
+      }
+
+      it("negative large") {
+        check("-0b1010_1010_1010_1010_1010_1010_1010_1010_1010L",
+          Some(success(Token.LongLiteral(-45812984490L), 0, 47)))
+      }
     }
 
-    it("Int.MinValue with leading zeroes") {
-      check("-00002147483648", Some(success(Token.IntLiteral(Int.MinValue), 0, 14)))
-    }
+    describe("hex") {
+      it("int (small)") {
+        check("0x1234_abcd", Some(success(Token.IntLiteral(0x1234abcd), 0, 10)))
+      }
 
-    it("Int.MinValue-1") {
-      check("-2147483649", Some(failure(IntegerNumberTooLarge, 0, 10)))
-    }
+      it("long (small)") {
+        check("0x1234_ABCDL", Some(success(Token.LongLiteral(0x1234abcd), 0, 11)))
+      }
 
-    it("Int.MaxValue") {
-      check("2147483647", Some(success(Token.IntLiteral(Int.MaxValue), 0, 9)))
-    }
+      it("large") {
+        check("0x1234_5678_abcd_EF01l", Some(success(Token.LongLiteral(0x1234_5678_abcd_ef01L), 0, 21)))
+      }
 
-    it("Int.MaxValue with leading zeroes") {
-      check("00002147483647", Some(success(Token.IntLiteral(Int.MaxValue), 0, 13)))
-    }
+      it("negative int (small)") {
+        check("-0x1234_AbCd", Some(success(Token.IntLiteral(-0x1234abcd), 0, 11)))
+      }
 
-    it("Int.MaxValue+1") {
-      check("2147483648", Some(failure(IntegerNumberTooLarge, 0, 9)))
-    }
-  }
+      it("negative long (small)") {
+        check("-0x1234_aBcDl", Some(success(Token.LongLiteral(-0x1234abcd), 0, 12)))
+      }
 
-  describe("binary") {
-
-    it("int (small)") {
-      check("0b1010_1010", Some(success(Token.IntLiteral(170), 0, 10)))
-    }
-
-    it("long (small)") {
-      check("0b1010_1010L", Some(success(Token.LongLiteral(170), 0, 11)))
-    }
-
-    it("large") {
-      check("0b1010_1010_1010_1010_1010_1010_1010_1010_1010L", Some(success(Token.LongLiteral(45812984490L), 0, 46)))
-    }
-
-    it("negative int (small)") {
-      check("-0b1010_1010", Some(success(Token.IntLiteral(-170), 0, 11)))
-    }
-
-    it("negative long (small)") {
-      check("-0b1010_1010L", Some(success(Token.LongLiteral(-170), 0, 12)))
-    }
-
-    it("negative large") {
-      check("-0b1010_1010_1010_1010_1010_1010_1010_1010_1010L",
-        Some(success(Token.LongLiteral(-45812984490L), 0, 47)))
-    }
-  }
-
-  describe("hex") {
-    it("int (small)") {
-      check("0x1234_abcd", Some(success(Token.IntLiteral(0x1234abcd), 0, 10)))
-    }
-
-    it("long (small)") {
-      check("0x1234_ABCDL", Some(success(Token.LongLiteral(0x1234abcd), 0, 11)))
-    }
-
-    it("large") {
-      check("0x1234_5678_abcd_EF01l", Some(success(Token.LongLiteral(0x1234_5678_abcd_ef01L), 0, 21)))
-    }
-
-    it("negative int (small)") {
-      check("-0x1234_AbCd", Some(success(Token.IntLiteral(-0x1234abcd), 0, 11)))
-    }
-
-    it("negative long (small)") {
-      check("-0x1234_aBcDl", Some(success(Token.LongLiteral(-0x1234abcd), 0, 12)))
-    }
-
-    it("negative (large)") {
-      check("-0x1234_5678_Abcd_eF01L",
-        Some(success(Token.LongLiteral(-0x1234_5678_abcd_ef01L), 0, 22)))
+      it("negative (large)") {
+        check("-0x1234_5678_Abcd_eF01L",
+          Some(success(Token.LongLiteral(-0x1234_5678_abcd_ef01L), 0, 22)))
+      }
     }
   }
 
