5786d5af65537c219ffd1f913098b3ce138d4075
Kevin Schuetz <schuetzk@gmail.com>
2026-02-28 00:26:35 -0600

    Refactor CharLiteralsTest check method to accept Pos[Token] directly

--- shared/src/test/scala/software/kes/scaletta/scanner/CharLiteralsTest.scala
+++ shared/src/test/scala/software/kes/scaletta/scanner/CharLiteralsTest.scala
@@ -12,114 +12,114 @@ class CharLiteralsTest extends AnyFunSpec with Matchers with AssertExpectedToken
 
   describe("charLiteral") {
     it("'a'") {
-      check("'a'", Some(success(Token.CharLiteral('a'), 0, 2)))
+      check("'a'", success(Token.CharLiteral('a'), 0, 2))
     }
 
     it("space") {
-      check("' '", Some(success(Token.CharLiteral(' '), 0, 2)))
+      check("' '", success(Token.CharLiteral(' '), 0, 2))
     }
 
     it("\\n") {
-      check("'\\n'", Some(success(Token.CharLiteral('\n'), 0, 3)))
+      check("'\\n'", success(Token.CharLiteral('\n'), 0, 3))
     }
 
     it("\\u0041") {
-      check("'\\u0041'", Some(success(Token.CharLiteral('A'), 0, 7)))
+      check("'\\u0041'", success(Token.CharLiteral('A'), 0, 7))
     }
 
     it("\\u21d2") {
-      check("'\\u21d2'", Some(success(Token.CharLiteral('⇒'), 0, 7)))
+      check("'\\u21d2'", success(Token.CharLiteral('⇒'), 0, 7))
     }
 
     it("⇒") {
-      check("'⇒'", Some(success(Token.CharLiteral('⇒'), 0, 2)))
+      check("'⇒'", success(Token.CharLiteral('⇒'), 0, 2))
     }
 
     it("unclosed 1") {
-      check("'a", Some(failure(ScannerError.UnclosedCharacterLiteral, 0, 1)))
+      check("'a", failure(ScannerError.UnclosedCharacterLiteral, 0, 1))
     }
 
     it("empty") {
-      check("''", Some(failure(ScannerError.EmptyCharacterLiteral, 0, 1)))
+      check("''", failure(ScannerError.EmptyCharacterLiteral, 0, 1))
     }
   }
 
   describe("stringLiteral") {
     describe("single-line") {
       it("empty string") {
-        check("\"\"", Some(success(Token.StringLiteral(""), 0, 1)))
+        check("\"\"", success(Token.StringLiteral(""), 0, 1))
       }
 
       it("simple string, no escapes") {
         val input = "\"this is a simple string\""
-        check(input, Some(success(Token.StringLiteral("this is a simple string"), 0, 24)))
+        check(input, success(Token.StringLiteral("this is a simple string"), 0, 24))
       }
 
       it("string with escapes") {
         val input = "\"this \\n string \\t has \\f escapes \\\\ \""
-        check(input, Some(success(Token.StringLiteral("this \n string \t has \f escapes \\ "), 0, 37)))
+        check(input, success(Token.StringLiteral("this \n string \t has \f escapes \\ "), 0, 37))
       }
 
       it("string with escaped quotes") {
         val input = "\"before \\\"quotes\\\" after\""
-        check(input, Some(success(Token.StringLiteral("before \"quotes\" after"), 0, 24)))
+        check(input, success(Token.StringLiteral("before \"quotes\" after"), 0, 24))
       }
 
       it("string with unicode sequences") {
         val input = "\"⇒ is the same as \\u21d2!\""
-        check(input, Some(success(Token.StringLiteral("⇒ is the same as ⇒!"), 0, 25)))
+        check(input, success(Token.StringLiteral("⇒ is the same as ⇒!"), 0, 25))
       }
     }
 
     describe("multi-line") {
       it("empty string") {
-        check("\"\"\"\"\"\"", Some(success(Token.MultiLineString(""), 0, 5)))
+        check("\"\"\"\"\"\"", success(Token.MultiLineString(""), 0, 5))
       }
 
       it("simple string, no new lines, no escapes") {
         val input = "\"\"\"this is a simple string\"\"\""
-        check(input, Some(success(Token.MultiLineString("this is a simple string"), 0, 28)))
+        check(input, success(Token.MultiLineString("this is a simple string"), 0, 28))
       }
 
       it("string with new lines and escapes") {
         val input = "\"\"\"line 1\nline 2\nline 3\\nthis\\tline\\fhas\\\\escapes \"\"\""
-        check(input, Some(success(Token.MultiLineString("line 1\nline 2\nline 3\nthis\tline\fhas\\escapes "), 0, 52)))
+        check(input, success(Token.MultiLineString("line 1\nline 2\nline 3\nthis\tline\fhas\\escapes "), 0, 52))
       }
 
       it("string with escaped quotes") {
         val input = "\"\"\"before \\\"quotes\\\" after\"\"\""
-        check(input, Some(success(Token.MultiLineString("before \"quotes\" after"), 0, 28)))
+        check(input, success(Token.MultiLineString("before \"quotes\" after"), 0, 28))
       }
 
       it("string with unescaped quotes") {
         val input = "\"\"\"before \"single\" \"\"double\"\" after\"\"\""
-        check(input, Some(success(Token.MultiLineString("before \"single\" \"\"double\"\" after"), 0, 37)))
+        check(input, success(Token.MultiLineString("before \"single\" \"\"double\"\" after"), 0, 37))
       }
 
       it("string with unicode sequences") {
         val input = "\"\"\"⇒ is the same as \\u21d2!\"\"\""
-        check(input, Some(success(Token.MultiLineString("⇒ is the same as ⇒!"), 0, 29)))
+        check(input, success(Token.MultiLineString("⇒ is the same as ⇒!"), 0, 29))
       }
     }
 
     it("unclosed single-line string") {
       val input = "\"abc"
-      check(input, Some(failure(ScannerError.UnclosedStringLiteral, 0, 3)))
+      check(input, failure(ScannerError.UnclosedStringLiteral, 0, 3))
     }
 
     it("unclosed multi-line string") {
       val input = "\"\"\"abc"
-      check(input, Some(failure(ScannerError.UnclosedMultiLineString, 0, 5)))
+      check(input, failure(ScannerError.UnclosedMultiLineString, 0, 5))
     }
   }
 
   private def check(input: String,
-                    expectedTokens: Option[Pos[Token]]*)
+                    expectedTokens: Pos[Token]*)
                    (implicit pos: Position): Unit = {
     TestReaderFactory.fromString(input) { reader =>
       val scanner = Scanner.create(reader, IdentifierPolicy.Default)
       val actualTokens = Iterator.continually(scanner.get()).takeWhile(_.value != Token.EndOfInput).toVector
-      assertExpectedTokens(input, expectedTokens.toVector.flatten, actualTokens)
+      assertExpectedTokens(input, expectedTokens.toVector, actualTokens)
     }
   }
 }
