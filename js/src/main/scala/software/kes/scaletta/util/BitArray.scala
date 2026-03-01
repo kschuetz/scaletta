78a33fe28e7fe76a3747af3c997d9496dbd6e36a
Kevin Schuetz <schuetzk@gmail.com>
2026-03-01 02:12:08 -0600

    Platform dependent implementations of BitArray

---
+++ js/src/main/scala/software/kes/scaletta/util/BitArray.scala
@@ -0,0 +1,51 @@
+package software.kes.scaletta.util
+
+object BitArray {
+  def create(initialBitCapacity: Int = 0): BitArray = {
+    if (initialBitCapacity <= 0) new BitArray(new Array[Int](0))
+    else new BitArray(new Array[Int]((initialBitCapacity + 31) / 32))
+  }
+}
+
+final class BitArray private(private var elements: Array[Int]) {
+  def get(index: Int): Boolean = {
+    val arrayIndex = index >>> 5
+    if (arrayIndex >= elements.length) false
+    else {
+      val bitIndex = index & 31
+      (elements(arrayIndex) & (1 << bitIndex)) != 0
+    }
+  }
+
+  def set(index: Int): Unit = {
+    ensureCapacity(index + 1)
+    val bitIndex = index & 31
+    val arrayIndex = index >>> 5
+    elements(arrayIndex) |= (1 << bitIndex)
+  }
+
+  def clear(index: Int): Unit = {
+    val arrayIndex = index >>> 5
+    if (arrayIndex < elements.length) {
+      val bitIndex = index & 31
+      elements(arrayIndex) &= ~(1 << bitIndex)
+    }
+  }
+
+  def update(index: Int, value: Boolean): Unit = {
+    if (value) set(index)
+    else clear(index)
+  }
+
+  def ensureCapacity(bitCapacity: Int): Unit = {
+    val requiredInts = (bitCapacity + 31) / 32
+    if (requiredInts > elements.length) {
+      val newCapacity = Math.max(requiredInts, elements.length * 2)
+      val newElements = new Array[Int](newCapacity)
+      System.arraycopy(elements, 0, newElements, 0, elements.length)
+      elements = newElements
+    }
+  }
+
+  def bitCapacity(): Int = elements.length * 32
+}
