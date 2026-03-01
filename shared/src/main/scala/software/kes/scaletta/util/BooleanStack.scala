a20a09c67a04ca18859b7170516869b717ed96ca
Kevin Schuetz <schuetzk@gmail.com>
2026-03-01 02:15:06 -0600

    BooleanStack now uses BitArray

--- shared/src/main/scala/software/kes/scaletta/util/BooleanStack.scala
+++ shared/src/main/scala/software/kes/scaletta/util/BooleanStack.scala
@@ -2,25 +2,18 @@ package software.kes.scaletta.util
 
 object BooleanStack {
   def create(): BooleanStack = {
-    new BooleanStack(new Array[Long](1), 0)
+    new BooleanStack(BitArray.create(64), 0)
   }
 }
 
-final class BooleanStack private(private var elements: Array[Long],
+final class BooleanStack private(private val elements: BitArray,
                                  private var _size: Int) {
   def size(): Int = _size
 
   def isEmpty: Boolean = _size == 0
 
   def push(value: Boolean): Unit = {
-    ensureCapacity()
-    val bitIndex = _size & 63
-    val arrayIndex = _size >>> 6
-    if (value) {
-      elements(arrayIndex) |= (1L << bitIndex)
-    } else {
-      elements(arrayIndex) &= ~(1L << bitIndex)
-    }
+    elements.update(_size, value)
     _size += 1
   }
 
@@ -28,11 +21,7 @@ final class BooleanStack private(private var elements: Array[Long],
     if (_size == 0) {
       None
     } else {
-      val index = _size - 1
-      val bitIndex = index & 63
-      val arrayIndex = index >>> 6
-      val value = (elements(arrayIndex) & (1L << bitIndex)) != 0
-      Some(value)
+      Some(elements.get(_size - 1))
     }
   }
 
@@ -40,20 +29,9 @@ final class BooleanStack private(private var elements: Array[Long],
     if (_size == 0) {
       throw new NoSuchElementException("pop() called on empty stack")
     } else {
-      val index = _size - 1
-      val bitIndex = index & 63
-      val arrayIndex = index >>> 6
-      val value = (elements(arrayIndex) & (1L << bitIndex)) != 0
+      val value = elements.get(_size - 1)
       _size -= 1
       value
-    }
-  }
-
-  private def ensureCapacity(): Unit = {
-    if (_size == elements.length * 64) {
-      val newElements = new Array[Long](elements.length * 2)
-      System.arraycopy(elements, 0, newElements, 0, elements.length)
-      elements = newElements
     }
   }
 }
