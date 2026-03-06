fc1e731315b1609faa866158523d8d339565acc2
Kevin Schuetz <schuetzk@gmail.com>
2026-03-06 00:20:07 -0600

    Reorganize ParserTest by grammar category

--- shared/src/test/scala/software/kes/scaletta/parser/ParserTest.scala
+++ shared/src/test/scala/software/kes/scaletta/parser/ParserTest.scala
@@ -11,61 +11,71 @@ import software.kes.scaletta.util.functional.~>
 
 class ParserTest extends AnyFunSpec with Matchers {
   describe("Parser") {
-    it("should parse an integer literal") {
-      parseValue("123") shouldBe lit(123)
+    describe("Literals") {
+      it("should parse an integer literal") {
+        parseValue("123") shouldBe lit(123)
+      }
+
+      it("should parse a string literal") {
+        parseValue("\"hello\"") shouldBe lit("hello")
+      }
+
+      it("should parse boolean literals") {
+        parseValue("true") shouldBe lit(true)
+        parseValue("false") shouldBe lit(false)
+      }
+
+      it("should parse null literal") {
+        parseValue("null") shouldBe litNull
+      }
     }
 
-    it("should parse a string literal") {
-      parseValue("\"hello\"") shouldBe lit("hello")
+    describe("Identifiers") {
+      it("should parse an identifier") {
+        parseValue("foo") shouldBe ref("foo")
+      }
     }
 
-    it("should parse boolean literals") {
-      parseValue("true") shouldBe lit(true)
-      parseValue("false") shouldBe lit(false)
-    }
+    describe("Expressions") {
+      describe("Arithmetic & Precedence") {
+        it("should parse infix addition") {
+          parseValue("1 + 2") shouldBe infix(lit(1), "+", lit(2))
+        }
 
-    it("should parse null literal") {
-      parseValue("null") shouldBe litNull
-    }
+        it("should respect operator precedence (1 + 2 * 3)") {
+          parseValue("1 + 2 * 3") shouldBe infix(lit(1), "+", infix(lit(2), "*", lit(3)))
+        }
 
-    it("should parse an identifier") {
-      parseValue("foo") shouldBe ref("foo")
-    }
+        it("should respect operator precedence (1 * 2 + 3)") {
+          parseValue("1 * 2 + 3") shouldBe infix(infix(lit(1), "*", lit(2)), "+", lit(3))
+        }
 
-    it("should parse infix addition") {
-      parseValue("1 + 2") shouldBe infix(lit(1), "+", lit(2))
-    }
+        it("should handle parentheses for grouping") {
+          parseValue("(1 + 2) * 3") shouldBe infix(infix(lit(1), "+", lit(2)), "*", lit(3))
+        }
+      }
 
-    it("should respect operator precedence (1 + 2 * 3)") {
-      parseValue("1 + 2 * 3") shouldBe infix(lit(1), "+", infix(lit(2), "*", lit(3)))
-    }
+      describe("Function Calls") {
+        it("should parse a standard function call (f(x, y))") {
+          parseValue("f(x, y)") shouldBe call(ref("f"), ref("x"), ref("y"))
+        }
 
-    it("should respect operator precedence (1 * 2 + 3)") {
-      parseValue("1 * 2 + 3") shouldBe infix(infix(lit(1), "*", lit(2)), "+", lit(3))
-    }
+        it("should parse a function call with no arguments (f())") {
+          parseValue("f()") shouldBe call(ref("f"))
+        }
 
-    it("should handle parentheses for grouping") {
-      parseValue("(1 + 2) * 3") shouldBe infix(infix(lit(1), "+", lit(2)), "*", lit(3))
-    }
+        it("should parse a nested function call (f(g(x)))") {
+          parseValue("f(g(x))") shouldBe call(ref("f"), call(ref("g"), ref("x")))
+        }
 
-    it("should parse a standard function call (f(x, y))") {
-      parseValue("f(x, y)") shouldBe call(ref("f"), ref("x"), ref("y"))
-    }
+        it("should parse a call on an expression target ((f + g)(x))") {
+          parseValue("(f + g)(x)") shouldBe call(infix(ref("f"), "+", ref("g")), ref("x"))
+        }
 
-    it("should parse a function call with no arguments (f())") {
-      parseValue("f()") shouldBe call(ref("f"))
-    }
-
-    it("should parse a nested function call (f(g(x)))") {
-      parseValue("f(g(x))") shouldBe call(ref("f"), call(ref("g"), ref("x")))
-    }
-
-    it("should parse a call on an expression target ((f + g)(x))") {
-      parseValue("(f + g)(x)") shouldBe call(infix(ref("f"), "+", ref("g")), ref("x"))
-    }
-
-    it("should parse multiple argument groups (f(x)(y))") {
-      parseValue("f(x)(y)") shouldBe multiCall(ref("f"), Vector(Vector(ref("x")), Vector(ref("y"))))
+        it("should parse multiple argument groups (f(x)(y))") {
+          parseValue("f(x)(y)") shouldBe multiCall(ref("f"), Vector(Vector(ref("x")), Vector(ref("y"))))
+        }
+      }
     }
   }
 
