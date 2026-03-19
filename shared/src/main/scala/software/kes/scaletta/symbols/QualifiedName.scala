aa69a193b77e0ded4c3fde738254e5306c1228a2
Kevin Schuetz <schuetzk@gmail.com>
2026-03-18 23:24:00 -0500

    QualifiedName

---
+++ shared/src/main/scala/software/kes/scaletta/symbols/QualifiedName.scala
@@ -0,0 +1,71 @@
+package software.kes.scaletta.symbols
+
+import software.kes.scaletta.common.{PackagePath, PackageSegment}
+
+sealed trait QualifiedName
+
+object QualifiedName {
+
+  /**
+   * Can throw IllegalArgumentException if the path is invalid.
+   * Use tryParseFull() if you want to handle errors.
+   */
+  def full(path: String): Full =
+    tryParseFull(path) match {
+      case Left(error) => throw new IllegalArgumentException(error)
+      case Right(result) => result
+    }
+
+  def full(qualifier: PackagePath.Absolute, name: String): Full =
+    Full(qualifier, name)
+
+  /**
+   * Can throw IllegalArgumentException if the path is invalid.
+   * Use tryParsePartial() if you want to handle errors.
+   */
+  def partial(path: String): Partial =
+    tryParsePartial(path) match {
+      case Left(error) => throw new IllegalArgumentException(error)
+      case Right(result) => result
+    }
+
+  def local(name: String): Partial =
+    Partial(None, name)
+
+  def tryParsePartial(path: String): Either[String, Partial] = {
+    val trimmed = path.trim
+    val lastDot = trimmed.lastIndexOf('.')
+    if (lastDot == -1) {
+      PackageSegment.parse(trimmed).map(name => Partial(None, name.name))
+    } else {
+      val (qualifierStr, nameStr) = trimmed.splitAt(lastDot)
+      for {
+        name <- PackageSegment.parse(nameStr.tail)
+        qualifier <- PackagePath.parse(qualifierStr)
+      } yield Partial(Some(qualifier), name.name)
+    }
+  }
+
+  def tryParseFull(path: String): Either[String, Full] = {
+    val trimmed = path.trim
+    val lastDot = trimmed.lastIndexOf('.')
+    if (lastDot == -1) {
+      PackageSegment.parse(trimmed).map(name => Full(PackagePath.root, name.name))
+    } else {
+      val (qualifierStr, nameStr) = trimmed.splitAt(lastDot)
+      for {
+        name <- PackageSegment.parse(nameStr.tail)
+        qualifier <- PackagePath.parseAbsolute(qualifierStr)
+      } yield Full(qualifier, name.name)
+    }
+  }
+
+  case class Full(qualifier: PackagePath.Absolute,
+                  name: String) extends QualifiedName
+
+  /**
+   * @param qualifier if None, it's a local name. If Some, the package path can be either Absolute or Relative.
+   */
+  case class Partial(qualifier: Option[PackagePath],
+                     name: String) extends QualifiedName
+}
---
+++ shared/src/test/scala/software/kes/scaletta/symbols/QualifiedNameSpec.scala
@@ -0,0 +1,62 @@
+package software.kes.scaletta.symbols
+
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+import software.kes.scaletta.common.PackagePath
+
+class QualifiedNameSpec extends AnyFunSpec with Matchers {
+  describe("QualifiedName") {
+    describe("tryParseFull") {
+      it("should parse a name in the root package (no dot)") {
+        val result = QualifiedName.tryParseFull("scaletta")
+        result shouldBe Right(QualifiedName.Full(PackagePath.root, "scaletta"))
+      }
+
+      it("should parse a name with _root_ prefix") {
+        val result = QualifiedName.tryParseFull("_root_.scaletta")
+        result shouldBe Right(QualifiedName.Full(PackagePath.root, "scaletta"))
+      }
+
+      it("should parse a multi-level qualified name") {
+        val result = QualifiedName.tryParseFull("org.example.User")
+        val expectedPkg = PackagePath.parseAbsolute("org.example").toOption.get
+        result shouldBe Right(QualifiedName.Full(expectedPkg, "User"))
+      }
+
+      it("should fail on empty string") {
+        QualifiedName.tryParseFull("") shouldBe a[Left[_, _]]
+      }
+
+      it("should fail on trailing dot") {
+        QualifiedName.tryParseFull("pkg.") shouldBe a[Left[_, _]]
+      }
+
+      it("should fail on invalid identifier") {
+        QualifiedName.tryParseFull("pkg.123") shouldBe a[Left[_, _]]
+      }
+    }
+
+    describe("tryParsePartial") {
+      it("should parse a local name (no dot)") {
+        val result = QualifiedName.tryParsePartial("x")
+        result shouldBe Right(QualifiedName.Partial(None, "x"))
+      }
+
+      it("should parse a relative qualified name") {
+        val result = QualifiedName.tryParsePartial("models.User")
+        val expectedPkg = PackagePath.parseRelative("models").toOption.get
+        result shouldBe Right(QualifiedName.Partial(Some(expectedPkg), "User"))
+      }
+
+      it("should parse an absolute qualified name") {
+        val result = QualifiedName.tryParsePartial("_root_.pkg.User")
+        val expectedPkg = PackagePath.parseAbsolute("pkg").toOption.get
+        result shouldBe Right(QualifiedName.Partial(Some(expectedPkg), "User"))
+      }
+
+      it("should fail on empty string") {
+        QualifiedName.tryParsePartial("") shouldBe a[Left[_, _]]
+      }
+    }
+  }
+}
