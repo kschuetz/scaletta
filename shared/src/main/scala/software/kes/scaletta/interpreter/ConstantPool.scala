69f19f1eb2125b0a17dbf417b047312bd1b6d3cb
Kevin Schuetz <schuetzk@gmail.com>
2026-03-27 21:24:42 -0500

    ConstantPool

---
+++ shared/src/main/scala/software/kes/scaletta/interpreter/ConstantPool.scala
@@ -0,0 +1,16 @@
+package software.kes.scaletta.interpreter
+
+import scala.collection.immutable.ArraySeq
+
+final class ConstantPool(val longs: ArraySeq[Long],
+                         val doubles: ArraySeq[Double],
+                         val floats: ArraySeq[Float],
+                         val objects: ArraySeq[Any]) {
+  def getObject(index: Int): Any = objects(index)
+
+  def getLong(index: Int): Long = longs(index)
+
+  def getDouble(index: Int): Double = doubles(index)
+
+  def getFloat(index: Int): Float = floats(index)
+}
