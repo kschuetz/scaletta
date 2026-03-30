1a143f23c6a9007d698450e8dda1605f01d46b76
Kevin Schuetz <schuetzk@gmail.com>
2026-03-29 23:33:43 -0500

    DoubleStack

---
+++ shared/src/main/scala/software/kes/scaletta/util/stack/DoubleStack.scala
@@ -0,0 +1,47 @@
+package software.kes.scaletta.util.stack
+
+import software.kes.scaletta.util.array.ArrayUtil
+
+object DoubleStack {
+  def create(initialCapacity: Int = 16): DoubleStack = {
+    new DoubleStack(new Array[Double](Math.max(initialCapacity, 1)))
+  }
+}
+
+final class DoubleStack private(private var elements: Array[Double]) extends PrimitiveStack {
+  def push(value: Double): Unit = {
+    ensureCapacity(_size + 1)
+    elements(_size) = value
+    _size += 1
+  }
+
+  def peek(): Option[Double] = {
+    if (_size == 0) {
+      None
+    } else {
+      Some(elements(_size - 1))
+    }
+  }
+
+  def pop(): Double = {
+    checkBeforePop()
+    val value = elements(_size - 1)
+    _size -= 1
+    value
+  }
+
+  /**
+   * Gets the value at the specified position from the top of the stack.
+   *
+   * @param position 0 is top of the stack, 1 is second from top, etc.
+   *                 position must be less than size, or the result is undefined.
+   */
+  def unsafeGet(position: Int): Double = {
+    val idx = _size - 1 - position
+    elements(idx)
+  }
+
+  private def ensureCapacity(minCapacity: Int): Unit = {
+    elements = ArrayUtil.growDoubleArray(elements, minCapacity, _size)
+  }
+}
---
+++ shared/src/test/scala/software/kes/scaletta/util/stack/DoubleStackSpec.scala
@@ -0,0 +1,133 @@
+package software.kes.scaletta.util.stack
+
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+
+class DoubleStackSpec extends AnyFunSpec with Matchers {
+  describe("DoubleStack") {
+    it("should be initially empty") {
+      val stack = DoubleStack.create()
+      stack.peek() shouldBe None
+      stack.isEmpty shouldBe true
+      stack.size() shouldBe 0
+    }
+
+    it("should push and peek values correctly") {
+      val stack = DoubleStack.create()
+      stack.push(41.0)
+      stack.peek() shouldBe Some(41.0)
+      stack.push(43.0)
+      stack.peek() shouldBe Some(43.0)
+    }
+
+    it("should ensure peek does not change the state of the stack") {
+      val stack = DoubleStack.create()
+      stack.push(41.0)
+      stack.push(43.0)
+
+      stack.peek() shouldBe Some(43.0)
+      stack.peek() shouldBe Some(43.0)
+
+      stack.pop() shouldBe 43.0
+      stack.peek() shouldBe Some(41.0)
+      stack.peek() shouldBe Some(41.0)
+    }
+
+    it("should push and pop values correctly (LIFO)") {
+      val stack = DoubleStack.create()
+      stack.push(10.5)
+      stack.push(20.5)
+      stack.push(30.5)
+
+      stack.pop() shouldBe 30.5
+      stack.peek() shouldBe Some(20.5)
+
+      stack.pop() shouldBe 20.5
+      stack.peek() shouldBe Some(10.5)
+
+      stack.pop() shouldBe 10.5
+      stack.peek() shouldBe None
+    }
+
+    it("should handle a sequence of pushes and pops") {
+      val stack = DoubleStack.create()
+      val values = Seq(1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7)
+
+      values.foreach(stack.push)
+
+      values.reverse.foreach { expected =>
+        stack.pop() shouldBe expected
+      }
+
+      stack.peek() shouldBe None
+    }
+
+    it("should support a large number of elements (testing growth)") {
+      val stack = DoubleStack.create(initialCapacity = 2)
+      val count = 1000
+      for (i <- 1 to count) {
+        stack.push(i.toDouble)
+      }
+
+      stack.size() shouldBe count
+
+      for (i <- count to 1 by -1) {
+        stack.pop() shouldBe i.toDouble
+      }
+      stack.peek() shouldBe None
+      stack.isEmpty shouldBe true
+    }
+
+    it("should throw an exception when popping from an empty stack") {
+      val stack = DoubleStack.create()
+      assertThrows[NoSuchElementException] {
+        stack.pop()
+      }
+    }
+
+    it("should correctly report isEmpty and size") {
+      val stack = DoubleStack.create()
+      stack.isEmpty shouldBe true
+      stack.size() shouldBe 0
+
+      stack.push(41.0)
+      stack.isEmpty shouldBe false
+      stack.size() shouldBe 1
+
+      stack.push(43.0)
+      stack.size() shouldBe 2
+
+      stack.pop()
+      stack.size() shouldBe 1
+      stack.isEmpty shouldBe false
+
+      stack.pop()
+      stack.isEmpty shouldBe true
+      stack.size() shouldBe 0
+    }
+
+    it("should correctly clear the stack") {
+      val stack = DoubleStack.create()
+      stack.push(41.0)
+      stack.push(43.0)
+      stack.size() shouldBe 2
+      stack.isEmpty shouldBe false
+
+      stack.clear()
+      stack.size() shouldBe 0
+      stack.isEmpty shouldBe true
+      stack.peek() shouldBe None
+    }
+
+    it("should support unsafeGet") {
+      val stack = DoubleStack.create()
+      stack.push(100.1) // index 2 from top
+      stack.push(200.2) // index 1 from top
+      stack.push(300.3) // index 0 from top
+
+      stack.unsafeGet(0) shouldBe 300.3
+      stack.unsafeGet(1) shouldBe 200.2
+      stack.unsafeGet(2) shouldBe 100.1
+    }
+  }
+}
