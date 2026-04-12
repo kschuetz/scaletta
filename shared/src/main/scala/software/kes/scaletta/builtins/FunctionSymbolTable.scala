ed55281abde89a12a5ec87c519819cf04c1c885b
Kevin Schuetz <schuetzk@gmail.com>
2026-04-12 00:35:41 -0500

    Rename FunctionTable to FunctionSymbolTable

--- shared/src/main/scala/software/kes/scaletta/builtins/FunctionSymbolTable.scala
+++ shared/src/main/scala/software/kes/scaletta/builtins/FunctionSymbolTable.scala
@@ -2,11 +2,11 @@ package software.kes.scaletta.builtins
 
 import software.kes.scaletta.symbols.{ImportScope, QualifiedName, SymbolEntry, SymbolTable}
 
-object FunctionTable {
-  def empty: FunctionTable = new FunctionTable(SymbolTable.empty)
+object FunctionSymbolTable {
+  def empty: FunctionSymbolTable = new FunctionSymbolTable(SymbolTable.empty)
 }
 
-final class FunctionTable private(private val table: SymbolTable[OverloadTable]) {
+final class FunctionSymbolTable private(private val table: SymbolTable[OverloadTable]) {
   def get(name: QualifiedName.Full): Option[OverloadTable] = table.get(name)
 
   def resolve(name: QualifiedName,
