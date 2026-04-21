e4740966b8bb69a8644c5c01d3b674cbf957ada1
Kevin Schuetz <schuetzk@gmail.com>
2026-04-20 22:21:06 -0500

    Add ReceiverType

--- shared/src/main/scala/software/kes/scaletta/api/MethodRegistry.scala
+++ shared/src/main/scala/software/kes/scaletta/api/MethodRegistry.scala
@@ -1,33 +1,21 @@
 package software.kes.scaletta.api
 
 import software.kes.scaletta.builtins.{FunctionImpl, NativeFunctionId, ParameterGroup}
-import software.kes.scaletta.symbols.{Name, QualifiedName}
+import software.kes.scaletta.symbols.Name
 import software.kes.scaletta.types.{Type, TypeId}
 
 trait MethodRegistry {
-  def addPureMethod(typ: Type.Nominal[TypeId],
+  def addPureMethod(receiverType: ReceiverType,
                     name: Name,
                     paramGroups: Vector[ParameterGroup],
                     returnType: Type[TypeId],
                     impl: FunctionImpl,
                     requireRuntimeContexts: Set[RuntimeContextId] = Set.empty): NativeFunctionId
 
-  def addPureFunction(name: QualifiedName.Full,
-                      paramGroups: Vector[ParameterGroup],
-                      returnType: Type[TypeId],
-                      impl: FunctionImpl,
-                      requireRuntimeContexts: Set[RuntimeContextId] = Set.empty): NativeFunctionId
-
-  def addImpureMethod(typ: Type.Nominal[TypeId],
+  def addImpureMethod(receiverType: ReceiverType,
                       name: Name,
                       paramGroups: Vector[ParameterGroup],
                       returnType: Type[TypeId],
                       impl: FunctionImpl,
                       requireRuntimeContexts: Set[RuntimeContextId] = Set.empty): NativeFunctionId
-
-  def addImpureFunction(name: QualifiedName.Full,
-                        paramGroups: Vector[ParameterGroup],
-                        returnType: Type[TypeId],
-                        impl: FunctionImpl,
-                        requireRuntimeContexts: Set[RuntimeContextId] = Set.empty): NativeFunctionId
 }
---
+++ shared/src/main/scala/software/kes/scaletta/api/ReceiverType.scala
@@ -0,0 +1,26 @@
+package software.kes.scaletta.api
+
+import software.kes.scaletta.common.PackagePath
+import software.kes.scaletta.types.{Type, TypeId}
+
+import scala.language.implicitConversions
+
+sealed trait ReceiverType
+
+object ReceiverType {
+  implicit def instance(typ: Type.Nominal[TypeId]): ReceiverType = Instance(typ)
+
+  implicit def static(path: PackagePath.Absolute): ReceiverType = Static(path)
+
+  /**
+   * A method on an instance of a type. The first parameter (index 0) of the method will be the instance,
+   * and the remaining parameters will be the method arguments. Actual method arguments will start at index 1.
+   */
+  case class Instance(typ: Type.Nominal[TypeId]) extends ReceiverType
+
+  /**
+   * A top-level method in a package. An instance argument will not be inserted before the method arguments.
+   * Actual method arguments will start at index 0.
+   */
+  case class Static(path: PackagePath.Absolute) extends ReceiverType
+}
