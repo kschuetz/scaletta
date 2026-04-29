bb8d5a73e91b8cd8a076cb3ae875996e9d80c869
Kevin Schuetz <schuetzk@gmail.com>
2026-04-28 23:21:21 -0500

    Initial AdjacencyTypeHierarchy implementation and tests

---
+++ shared/src/main/scala/software/kes/scaletta/types/AdjacencyTypeHierarchy.scala
@@ -0,0 +1,101 @@
+package software.kes.scaletta.types
+
+import scala.annotation.tailrec
+import scala.collection.immutable.Queue
+
+object AdjacencyTypeHierarchy {
+  /**
+   * Creates an empty [[AdjacencyTypeHierarchy]].
+   */
+  def empty[T]: AdjacencyTypeHierarchy[T] = new AdjacencyTypeHierarchy(Map.empty)
+
+  /**
+   * Creates an [[AdjacencyTypeHierarchy]] from the given supertype mappings.
+   */
+  def fromMap[T](supertypes: Map[Type[T], Set[Type[T]]]): AdjacencyTypeHierarchy[T] =
+    new AdjacencyTypeHierarchy(supertypes)
+}
+
+/**
+ * A concrete implementation of [[TypeHierarchy]] that uses an adjacency map
+ * to represent the inheritance graph.
+ *
+ * @tparam T The type of the identifier used for types (e.g., [[TypeId]]).
+ */
+final class AdjacencyTypeHierarchy[T] private(private val supertypes: Map[Type[T], Set[Type[T]]]) extends TypeHierarchy[T] {
+
+  def relationshipFor(lhs: Type[T], rhs: Type[T]): TypeRelationship[T] = {
+    if (lhs == rhs) {
+      TypeRelationship.Same
+    } else if (isSubtypeOf(lhs, rhs)) {
+      TypeRelationship.StrictSubtype
+    } else if (isSubtypeOf(rhs, lhs)) {
+      TypeRelationship.StrictSupertype
+    } else {
+      findCommonSupertype(lhs, rhs).fold[TypeRelationship[T]](TypeRelationship.Unrelated)(TypeRelationship.HaveCommonSupertype(_))
+    }
+  }
+
+  def immediateSupertypes(t: Type[T]): Iterable[Type[T]] =
+    supertypes.getOrElse(t, Set.empty)
+
+  /**
+   * Checks if `lhs` is a subtype of `rhs`.
+   */
+  private def isSubtypeOf(lhs: Type[T], rhs: Type[T]): Boolean = {
+    @tailrec
+    def go(queue: Queue[Type[T]], visited: Set[Type[T]]): Boolean = {
+      queue.dequeueOption match {
+        case Some((current, rest)) =>
+          if (current == rhs) {
+            true
+          } else {
+            val nextSupertypes = immediateSupertypes(current).filterNot(visited.contains)
+            go(rest.enqueueAll(nextSupertypes), visited ++ nextSupertypes)
+          }
+        case None => false
+      }
+    }
+
+    go(Queue(lhs), Set(lhs))
+  }
+
+  /**
+   * Finds the least upper bound (LUB) of two types.
+   * Currently, it returns the first common supertype found during BFS.
+   */
+  private def findCommonSupertype(lhs: Type[T], rhs: Type[T]): Option[Type[T]] = {
+    val lhsAncestors = allAncestors(lhs)
+
+    @tailrec
+    def go(queue: Queue[Type[T]], visited: Set[Type[T]]): Option[Type[T]] = {
+      queue.dequeueOption match {
+        case Some((current, rest)) =>
+          if (lhsAncestors.contains(current)) {
+            Some(current)
+          } else {
+            val nextSupertypes = immediateSupertypes(current).filterNot(visited.contains)
+            go(rest.enqueueAll(nextSupertypes), visited ++ nextSupertypes)
+          }
+        case None => None
+      }
+    }
+
+    go(Queue(rhs), Set(rhs))
+  }
+
+  private def allAncestors(t: Type[T]): Set[Type[T]] = {
+    @tailrec
+    def go(queue: Queue[Type[T]], visited: Set[Type[T]], acc: Set[Type[T]]): Set[Type[T]] = {
+      queue.dequeueOption match {
+        case Some((current, rest)) =>
+          val nextSupertypes = immediateSupertypes(current).filterNot(visited.contains)
+          go(rest.enqueueAll(nextSupertypes), visited ++ nextSupertypes, acc + current)
+        case None => acc
+      }
+    }
+
+    go(Queue(t), Set(t), Set.empty)
+  }
+
+}
---
+++ shared/src/test/scala/software/kes/scaletta/types/AdjacencyTypeHierarchySpec.scala
@@ -0,0 +1,74 @@
+package software.kes.scaletta.types
+
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+
+class AdjacencyTypeHierarchySpec extends AnyFunSpec with Matchers {
+  describe("AdjacencyTypeHierarchy") {
+    it("should identify identical types as Same") {
+      hierarchy.relationshipFor(toNominal(ChildA1), toNominal(ChildA1)) shouldBe TypeRelationship.Same
+      hierarchy.relationshipFor(toNominal(Root), toNominal(Root)) shouldBe TypeRelationship.Same
+    }
+
+    it("should identify strict subtypes") {
+      hierarchy.relationshipFor(toNominal(ChildA1), toNominal(ParentA)) shouldBe TypeRelationship.StrictSubtype
+      hierarchy.relationshipFor(toNominal(ChildA1), toNominal(Root)) shouldBe TypeRelationship.StrictSubtype
+      hierarchy.relationshipFor(toNominal(ParentA), toNominal(Root)) shouldBe TypeRelationship.StrictSubtype
+      hierarchy.relationshipFor(toNominal(DiamondChild), toNominal(Root)) shouldBe TypeRelationship.StrictSubtype
+    }
+
+    it("should identify strict supertypes") {
+      hierarchy.relationshipFor(toNominal(ParentA), toNominal(ChildA1)) shouldBe TypeRelationship.StrictSupertype
+      hierarchy.relationshipFor(toNominal(Root), toNominal(ChildA1)) shouldBe TypeRelationship.StrictSupertype
+      hierarchy.relationshipFor(toNominal(Root), toNominal(ParentA)) shouldBe TypeRelationship.StrictSupertype
+      hierarchy.relationshipFor(toNominal(Root), toNominal(DiamondChild)) shouldBe TypeRelationship.StrictSupertype
+    }
+
+    it("should identify types with a common supertype (LUB)") {
+      hierarchy.relationshipFor(toNominal(ChildA1), toNominal(ParentB)) shouldBe TypeRelationship.HaveCommonSupertype(toNominal(Root))
+      hierarchy.relationshipFor(toNominal(ParentA), toNominal(ParentB)) shouldBe TypeRelationship.HaveCommonSupertype(toNominal(Root))
+      hierarchy.relationshipFor(toNominal(ChildA1), toNominal(ChildB1)) shouldBe TypeRelationship.HaveCommonSupertype(toNominal(Root))
+    }
+
+    it("should return Unrelated for unrelated types") {
+      hierarchy.relationshipFor(toNominal(Root), toNominal(Unrelated)) shouldBe TypeRelationship.Unrelated
+      hierarchy.relationshipFor(toNominal(ChildA1), toNominal(Unrelated)) shouldBe TypeRelationship.Unrelated
+    }
+
+    describe("immediateSupertypes") {
+      it("should return correct immediate supertypes") {
+        hierarchy.immediateSupertypes(toNominal(DiamondChild)) should contain theSameElementsAs Set(toNominal(ChildA1), toNominal(ChildB1))
+        hierarchy.immediateSupertypes(toNominal(ChildA1)) should contain theSameElementsAs Set(toNominal(ParentA))
+        hierarchy.immediateSupertypes(toNominal(Root)) shouldBe empty
+      }
+    }
+  }
+
+  sealed trait TestType
+
+  case object Root extends TestType
+
+  case object ParentA extends TestType
+
+  case object ParentB extends TestType
+
+  case object ChildA1 extends TestType
+
+  case object ChildB1 extends TestType
+
+  case object DiamondChild extends TestType
+
+  case object Unrelated extends TestType
+
+  private def toNominal(t: TestType): Type[TestType] = Type.Nominal(t)
+
+  private lazy val hierarchyMap: Map[Type[TestType], Set[Type[TestType]]] = Map(
+    toNominal(DiamondChild) -> Set(toNominal(ChildA1), toNominal(ChildB1)),
+    toNominal(ChildA1) -> Set(toNominal(ParentA)),
+    toNominal(ChildB1) -> Set(toNominal(ParentB)),
+    toNominal(ParentA) -> Set(toNominal(Root)),
+    toNominal(ParentB) -> Set(toNominal(Root))
+  )
+
+  private lazy val hierarchy = AdjacencyTypeHierarchy.fromMap(hierarchyMap)
+}
