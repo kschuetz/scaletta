362ca43fdd14afe9856c5deda4d1fa5d26c6413c
Kevin Schuetz <schuetzk@gmail.com>
2026-06-10 22:57:44 -0500

    InterpreterComplexExampleSpec: fibonacci

--- shared/src/test/scala/software/kes/scaletta/internal/interpreter/InterpreterComplexExampleSpec.scala
+++ shared/src/test/scala/software/kes/scaletta/internal/interpreter/InterpreterComplexExampleSpec.scala
@@ -1,6 +1,5 @@
 package software.kes.scaletta.internal.interpreter
 
-import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 import software.kes.scaletta.api.Scaletta
@@ -15,8 +14,7 @@ class InterpreterComplexExampleSpec extends AnyFunSuite with Matchers {
     .asInstanceOf[ScalettaFacade]
   private val stdLib = StandardLibraryLookup.create(scaletta.universe)
 
-  import stdLib.arithmetic
-  import stdLib.comparison
+  import stdLib.{arithmetic, comparison}
 
   private val nativeFunctions = scaletta.universe.methodUniverse.dispatchTable
 
@@ -75,6 +73,7 @@ class InterpreterComplexExampleSpec extends AnyFunSuite with Matchers {
 
     // Test cases
     val testCases = Seq(
+      -1 -> 1,
       0 -> 1,
       1 -> 1,
       2 -> 2,
@@ -82,7 +81,97 @@ class InterpreterComplexExampleSpec extends AnyFunSuite with Matchers {
       4 -> 24,
       5 -> 120,
       6 -> 720,
-      -1 -> 1
+    )
+
+    testCases.foreach { case (input, expected) =>
+      val initializer = Initializer { vs =>
+        vs.unsafeWriteInt(nVar, input)
+      }
+      val result = interpreter.run(emptyContextReader, initializer)
+      result.intValue() shouldBe expected
+    }
+  }
+
+  test("fibonacci (iterative)") {
+    val frame = FrameSignature.fromSeq(Seq(CoreTypes.IntT, CoreTypes.IntT, CoreTypes.IntT))
+    val signature = VarSpaceSignature.of(frame)
+    val builder = ProgramBuilder.create(BasicTypes.Int, signature)
+    val assembler = builder.mainAssembler()
+
+    // Variable layout:
+    // var 0: input 'n' (remaining steps)
+    // var 1: 'a' (current Fibonacci number)
+    // var 2: 'b' (next Fibonacci number)
+    val nVar = 0
+    val aVar = 1
+    val bVar = 2
+
+    val loopStart = assembler.label()
+    val loopExit = assembler.label()
+
+    // a = 0
+    assembler.pushImmediateInt(0)
+    assembler.popIntIntoVar(aVar)
+
+    // b = 1
+    assembler.pushImmediateInt(1)
+    assembler.popIntIntoVar(bVar)
+
+    // loopStart:
+    loopStart.bind()
+
+    // if (n <= 0) goto loopExit
+    assembler.pushIntFromVar(nVar)
+    assembler.pushImmediateInt(0)
+    assembler.callNative(comparison.int.le.int)
+    assembler.branchIf(loopExit)
+
+    // temp = a + b
+    assembler.pushIntFromVar(aVar)
+    assembler.pushIntFromVar(bVar)
+    assembler.callNative(arithmetic.int.add.int)
+    // We'll use the stack to hold 'temp' temporarily while we shift a and b
+
+    // a = b
+    assembler.pushIntFromVar(bVar)
+    assembler.popIntIntoVar(aVar)
+
+    // b = temp (from stack)
+    assembler.popIntIntoVar(bVar)
+
+    // n = n - 1
+    assembler.pushIntFromVar(nVar)
+    assembler.pushImmediateInt(1)
+    assembler.callNative(arithmetic.int.subtract.int)
+    assembler.popIntIntoVar(nVar)
+
+    // goto loopStart
+    assembler.branch(loopStart)
+
+    // loopExit:
+    loopExit.bind()
+
+    // return a
+    assembler.pushIntFromVar(aVar)
+    assembler.emitReturn()
+
+    val program = builder.build()
+    val interpreter = Interpreter.create(program, nativeFunctions)
+
+    // Test cases (n -> fib(n))
+    val testCases = Seq(
+      -1 -> 0,
+      0 -> 0,
+      1 -> 1,
+      2 -> 1,
+      3 -> 2,
+      4 -> 3,
+      5 -> 5,
+      6 -> 8,
+      7 -> 13,
+      8 -> 21,
+      9 -> 34,
+      10 -> 55,
     )
 
     testCases.foreach { case (input, expected) =>
