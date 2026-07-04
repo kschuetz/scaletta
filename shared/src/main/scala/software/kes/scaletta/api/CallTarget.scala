91111152840a7cf0d08ae29e7863922d5f16fb52
Kevin Schuetz <schuetzk@gmail.com>
2026-07-04 00:01:20 -0500

    Simplify CallTarget API

--- shared/src/main/scala/software/kes/scaletta/api/CallTarget.scala
+++ shared/src/main/scala/software/kes/scaletta/api/CallTarget.scala
@@ -2,22 +2,4 @@ package software.kes.scaletta.api
 
 trait CallTarget {
   def setArgument(index: Int, value: Any): Unit
-
-  def setArgumentObject(index: Int, value: AnyRef): Unit
-
-  def setArgumentInt(index: Int, value: Int): Unit
-
-  def setArgumentLong(index: Int, value: Long): Unit
-
-  def setArgumentFloat(index: Int, value: Float): Unit
-
-  def setArgumentDouble(index: Int, value: Double): Unit
-
-  def setArgumentBoolean(index: Int, value: Boolean): Unit
-
-  def setArgumentChar(index: Int, value: Char): Unit
-
-  def setArgumentShort(index: Int, value: Short): Unit
-
-  def setArgumentByte(index: Int, value: Byte): Unit
 }
