98ba42316ba7a276cf203bf9009f736150d259e9
Kevin Schuetz <schuetzk@gmail.com>
2026-07-11 01:43:35 -0500

    Optimizations in readAsX methods

--- shared/src/main/scala/software/kes/scaletta/internal/interpreter/OperandStackArgumentReader.scala
+++ shared/src/main/scala/software/kes/scaletta/internal/interpreter/OperandStackArgumentReader.scala
@@ -48,16 +48,11 @@ private[interpreter] class OperandStackArgumentReader(stack: OperandStack,
    * Safer than [[unsafeReadBoolean]] but slower.
    */
   def readAsBoolean(index: Int): Boolean = {
-    signature.basicTypeOf(index) match {
-      case BasicTypes.Boolean => unsafeReadBoolean(index)
-      case BasicTypes.Int => unsafeReadInt(index) != 0
-      case BasicTypes.Long => unsafeReadLong(index) != 0L
-      case BasicTypes.Short => unsafeReadShort(index) != 0
-      case BasicTypes.Byte => unsafeReadByte(index) != 0
-      case BasicTypes.Char => unsafeReadChar(index) != 0
-      case BasicTypes.Double => unsafeReadDouble(index) != 0.0d
-      case BasicTypes.Float => unsafeReadFloat(index) != 0.0f
-      case _ => ObjectToPrimitive.objectToBoolean(unsafeReadObject(index))
+    val basicType = signature.basicTypeOf(index)
+    if (basicType == BasicTypes.Boolean) {
+      unsafeReadBoolean(index)
+    } else {
+      readAndConvertToBoolean(index, basicType)
     }
   }
 
@@ -66,16 +61,11 @@ private[interpreter] class OperandStackArgumentReader(stack: OperandStack,
    * Safer than [[unsafeReadInt]] but slower.
    */
   def readAsInt(index: Int): Int = {
-    signature.basicTypeOf(index) match {
-      case BasicTypes.Int => unsafeReadInt(index)
-      case BasicTypes.Long => unsafeReadLong(index).toInt
-      case BasicTypes.Short => unsafeReadShort(index).toInt
-      case BasicTypes.Byte => unsafeReadByte(index).toInt
-      case BasicTypes.Char => unsafeReadChar(index).toInt
-      case BasicTypes.Double => unsafeReadDouble(index).toInt
-      case BasicTypes.Float => unsafeReadFloat(index).toInt
-      case BasicTypes.Boolean => if (unsafeReadBoolean(index)) 1 else 0
-      case _ => ObjectToPrimitive.objectToInt(unsafeReadObject(index))
+    val basicType = signature.basicTypeOf(index)
+    if (basicType == BasicTypes.Int) {
+      unsafeReadInt(index)
+    } else {
+      readAndConvertToInt(index, basicType)
     }
   }
 
@@ -84,16 +74,11 @@ private[interpreter] class OperandStackArgumentReader(stack: OperandStack,
    * Safer than [[unsafeReadLong]] but slower.
    */
   def readAsLong(index: Int): Long = {
-    signature.basicTypeOf(index) match {
-      case BasicTypes.Long => unsafeReadLong(index)
-      case BasicTypes.Int => unsafeReadInt(index).toLong
-      case BasicTypes.Short => unsafeReadShort(index).toLong
-      case BasicTypes.Byte => unsafeReadByte(index).toLong
-      case BasicTypes.Char => unsafeReadChar(index).toLong
-      case BasicTypes.Double => unsafeReadDouble(index).toLong
-      case BasicTypes.Float => unsafeReadFloat(index).toLong
-      case BasicTypes.Boolean => if (unsafeReadBoolean(index)) 1L else 0L
-      case _ => ObjectToPrimitive.objectToLong(unsafeReadObject(index))
+    val basicType = signature.basicTypeOf(index)
+    if (basicType == BasicTypes.Long) {
+      unsafeReadLong(index)
+    } else {
+      readAndConvertToLong(index, basicType)
     }
   }
 
@@ -102,16 +87,11 @@ private[interpreter] class OperandStackArgumentReader(stack: OperandStack,
    * Safer than [[unsafeReadShort]] but slower.
    */
   def readAsShort(index: Int): Short = {
-    signature.basicTypeOf(index) match {
-      case BasicTypes.Short => unsafeReadShort(index)
-      case BasicTypes.Int => unsafeReadInt(index).toShort
-      case BasicTypes.Long => unsafeReadLong(index).toShort
-      case BasicTypes.Byte => unsafeReadByte(index).toShort
-      case BasicTypes.Char => unsafeReadChar(index).toShort
-      case BasicTypes.Double => unsafeReadDouble(index).toShort
-      case BasicTypes.Float => unsafeReadFloat(index).toShort
-      case BasicTypes.Boolean => if (unsafeReadBoolean(index)) 1.toShort else 0.toShort
-      case _ => ObjectToPrimitive.objectToShort(unsafeReadObject(index))
+    val basicType = signature.basicTypeOf(index)
+    if (basicType == BasicTypes.Short) {
+      unsafeReadShort(index)
+    } else {
+      readAndConvertToShort(index, basicType)
     }
   }
 
@@ -120,16 +100,11 @@ private[interpreter] class OperandStackArgumentReader(stack: OperandStack,
    * Safer than [[unsafeReadByte]] but slower.
    */
   def readAsByte(index: Int): Byte = {
-    signature.basicTypeOf(index) match {
-      case BasicTypes.Byte => unsafeReadByte(index)
-      case BasicTypes.Int => unsafeReadInt(index).toByte
-      case BasicTypes.Long => unsafeReadLong(index).toByte
-      case BasicTypes.Short => unsafeReadShort(index).toByte
-      case BasicTypes.Char => unsafeReadChar(index).toByte
-      case BasicTypes.Double => unsafeReadDouble(index).toByte
-      case BasicTypes.Float => unsafeReadFloat(index).toByte
-      case BasicTypes.Boolean => if (unsafeReadBoolean(index)) 1.toByte else 0.toByte
-      case _ => ObjectToPrimitive.objectToByte(unsafeReadObject(index))
+    val basicType = signature.basicTypeOf(index)
+    if (basicType == BasicTypes.Byte) {
+      unsafeReadByte(index)
+    } else {
+      readAndConvertToByte(index, basicType)
     }
   }
 
@@ -138,16 +113,11 @@ private[interpreter] class OperandStackArgumentReader(stack: OperandStack,
    * Safer than [[unsafeReadChar]] but slower.
    */
   def readAsChar(index: Int): Char = {
-    signature.basicTypeOf(index) match {
-      case BasicTypes.Char => unsafeReadChar(index)
-      case BasicTypes.Int => unsafeReadInt(index).toChar
-      case BasicTypes.Long => unsafeReadLong(index).toChar
-      case BasicTypes.Short => unsafeReadShort(index).toChar
-      case BasicTypes.Byte => unsafeReadByte(index).toChar
-      case BasicTypes.Double => unsafeReadDouble(index).toChar
-      case BasicTypes.Float => unsafeReadFloat(index).toChar
-      case BasicTypes.Boolean => if (unsafeReadBoolean(index)) 1.toChar else 0.toChar
-      case _ => ObjectToPrimitive.objectToChar(unsafeReadObject(index))
+    val basicType = signature.basicTypeOf(index)
+    if (basicType == BasicTypes.Char) {
+      unsafeReadChar(index)
+    } else {
+      readAndConvertToChar(index, basicType)
     }
   }
 
@@ -156,16 +126,11 @@ private[interpreter] class OperandStackArgumentReader(stack: OperandStack,
    * Safer than [[unsafeReadDouble]] but slower.
    */
   def readAsDouble(index: Int): Double = {
-    signature.basicTypeOf(index) match {
-      case BasicTypes.Double => unsafeReadDouble(index)
-      case BasicTypes.Int => unsafeReadInt(index).toDouble
-      case BasicTypes.Long => unsafeReadLong(index).toDouble
-      case BasicTypes.Short => unsafeReadShort(index).toDouble
-      case BasicTypes.Byte => unsafeReadByte(index).toDouble
-      case BasicTypes.Char => unsafeReadChar(index).toDouble
-      case BasicTypes.Float => unsafeReadFloat(index).toDouble
-      case BasicTypes.Boolean => if (unsafeReadBoolean(index)) 1.0d else 0.0d
-      case _ => ObjectToPrimitive.objectToDouble(unsafeReadObject(index))
+    val basicType = signature.basicTypeOf(index)
+    if (basicType == BasicTypes.Double) {
+      unsafeReadDouble(index)
+    } else {
+      readAndConvertToDouble(index, basicType)
     }
   }
 
@@ -174,16 +139,11 @@ private[interpreter] class OperandStackArgumentReader(stack: OperandStack,
    * Safer than [[unsafeReadFloat]] but slower.
    */
   def readAsFloat(index: Int): Float = {
-    signature.basicTypeOf(index) match {
-      case BasicTypes.Float => unsafeReadFloat(index)
-      case BasicTypes.Int => unsafeReadInt(index).toFloat
-      case BasicTypes.Long => unsafeReadLong(index).toFloat
-      case BasicTypes.Short => unsafeReadShort(index).toFloat
-      case BasicTypes.Byte => unsafeReadByte(index).toFloat
-      case BasicTypes.Char => unsafeReadChar(index).toFloat
-      case BasicTypes.Double => unsafeReadDouble(index).toFloat
-      case BasicTypes.Boolean => if (unsafeReadBoolean(index)) 1.0f else 0.0f
-      case _ => ObjectToPrimitive.objectToFloat(unsafeReadObject(index))
+    val basicType = signature.basicTypeOf(index)
+    if (basicType == BasicTypes.Float) {
+      unsafeReadFloat(index)
+    } else {
+      readAndConvertToFloat(index, basicType)
     }
   }
 
@@ -191,8 +151,14 @@ private[interpreter] class OperandStackArgumentReader(stack: OperandStack,
    * Reads the argument at index and converts it to an object.
    * Safer than [[unsafeReadObject]] but slower.
    */
-  def readAsObject(index: Int): AnyRef =
-    read(index).asInstanceOf[AnyRef]
+  def readAsObject(index: Int): AnyRef = {
+    val basicType = signature.basicTypeOf(index)
+    if (basicType == BasicTypes.Object) {
+      unsafeReadObject(index)
+    } else {
+      read(index).asInstanceOf[AnyRef]
+    }
+  }
 
   def unsafeReadBoolean(index: Int): Boolean =
     stack.booleans.unsafeRead(signature.stackOffsetOf(index))
@@ -244,4 +210,100 @@ private[interpreter] class OperandStackArgumentReader(stack: OperandStack,
 
   def unsafeReadShortArray(index: Int): ArraySeq[Short] =
     unsafeReadObject(index).asInstanceOf[ArraySeq[Short]]
+
+  private def readAndConvertToBoolean(index: Int, incomingType: Byte): Boolean =
+    incomingType match {
+      case BasicTypes.Int => unsafeReadInt(index) != 0
+      case BasicTypes.Long => unsafeReadLong(index) != 0L
+      case BasicTypes.Short => unsafeReadShort(index) != 0
+      case BasicTypes.Byte => unsafeReadByte(index) != 0
+      case BasicTypes.Char => unsafeReadChar(index) != 0
+      case BasicTypes.Double => unsafeReadDouble(index) != 0.0d
+      case BasicTypes.Float => unsafeReadFloat(index) != 0.0f
+      case _ => ObjectToPrimitive.objectToBoolean(unsafeReadObject(index))
+    }
+
+  private def readAndConvertToInt(index: Int, incomingType: Byte): Int =
+    incomingType match {
+      case BasicTypes.Long => unsafeReadLong(index).toInt
+      case BasicTypes.Short => unsafeReadShort(index).toInt
+      case BasicTypes.Byte => unsafeReadByte(index).toInt
+      case BasicTypes.Char => unsafeReadChar(index).toInt
+      case BasicTypes.Double => unsafeReadDouble(index).toInt
+      case BasicTypes.Float => unsafeReadFloat(index).toInt
+      case BasicTypes.Boolean => if (unsafeReadBoolean(index)) 1 else 0
+      case _ => ObjectToPrimitive.objectToInt(unsafeReadObject(index))
+    }
+
+  private def readAndConvertToLong(index: Int, incomingType: Byte): Long =
+    incomingType match {
+      case BasicTypes.Int => unsafeReadInt(index).toLong
+      case BasicTypes.Short => unsafeReadShort(index).toLong
+      case BasicTypes.Byte => unsafeReadByte(index).toLong
+      case BasicTypes.Char => unsafeReadChar(index).toLong
+      case BasicTypes.Double => unsafeReadDouble(index).toLong
+      case BasicTypes.Float => unsafeReadFloat(index).toLong
+      case BasicTypes.Boolean => if (unsafeReadBoolean(index)) 1L else 0L
+      case _ => ObjectToPrimitive.objectToLong(unsafeReadObject(index))
+    }
+
+  private def readAndConvertToShort(index: Int, incomingType: Byte): Short =
+    incomingType match {
+      case BasicTypes.Int => unsafeReadInt(index).toShort
+      case BasicTypes.Long => unsafeReadLong(index).toShort
+      case BasicTypes.Byte => unsafeReadByte(index).toShort
+      case BasicTypes.Char => unsafeReadChar(index).toShort
+      case BasicTypes.Double => unsafeReadDouble(index).toShort
+      case BasicTypes.Float => unsafeReadFloat(index).toShort
+      case BasicTypes.Boolean => if (unsafeReadBoolean(index)) 1.toShort else 0.toShort
+      case _ => ObjectToPrimitive.objectToShort(unsafeReadObject(index))
+    }
+
+  private def readAndConvertToByte(index: Int, incomingType: Byte): Byte =
+    incomingType match {
+      case BasicTypes.Int => unsafeReadInt(index).toByte
+      case BasicTypes.Long => unsafeReadLong(index).toByte
+      case BasicTypes.Short => unsafeReadShort(index).toByte
+      case BasicTypes.Char => unsafeReadChar(index).toByte
+      case BasicTypes.Double => unsafeReadDouble(index).toByte
+      case BasicTypes.Float => unsafeReadFloat(index).toByte
+      case BasicTypes.Boolean => if (unsafeReadBoolean(index)) 1.toByte else 0.toByte
+      case _ => ObjectToPrimitive.objectToByte(unsafeReadObject(index))
+    }
+
+  private def readAndConvertToChar(index: Int, incomingType: Byte): Char =
+    incomingType match {
+      case BasicTypes.Int => unsafeReadInt(index).toChar
+      case BasicTypes.Long => unsafeReadLong(index).toChar
+      case BasicTypes.Short => unsafeReadShort(index).toChar
+      case BasicTypes.Byte => unsafeReadByte(index).toChar
+      case BasicTypes.Double => unsafeReadDouble(index).toChar
+      case BasicTypes.Float => unsafeReadFloat(index).toChar
+      case BasicTypes.Boolean => if (unsafeReadBoolean(index)) 1.toChar else 0.toChar
+      case _ => ObjectToPrimitive.objectToChar(unsafeReadObject(index))
+    }
+
+  private def readAndConvertToDouble(index: Int, incomingType: Byte): Double =
+    incomingType match {
+      case BasicTypes.Int => unsafeReadInt(index).toDouble
+      case BasicTypes.Long => unsafeReadLong(index).toDouble
+      case BasicTypes.Short => unsafeReadShort(index).toDouble
+      case BasicTypes.Byte => unsafeReadByte(index).toDouble
+      case BasicTypes.Char => unsafeReadChar(index).toDouble
+      case BasicTypes.Float => unsafeReadFloat(index).toDouble
+      case BasicTypes.Boolean => if (unsafeReadBoolean(index)) 1.0d else 0.0d
+      case _ => ObjectToPrimitive.objectToDouble(unsafeReadObject(index))
+    }
+
+  private def readAndConvertToFloat(index: Int, incomingType: Byte): Float =
+    incomingType match {
+      case BasicTypes.Int => unsafeReadInt(index).toFloat
+      case BasicTypes.Long => unsafeReadLong(index).toFloat
+      case BasicTypes.Short => unsafeReadShort(index).toFloat
+      case BasicTypes.Byte => unsafeReadByte(index).toFloat
+      case BasicTypes.Char => unsafeReadChar(index).toFloat
+      case BasicTypes.Double => unsafeReadDouble(index).toFloat
+      case BasicTypes.Boolean => if (unsafeReadBoolean(index)) 1.0f else 0.0f
+      case _ => ObjectToPrimitive.objectToFloat(unsafeReadObject(index))
+    }
 }
