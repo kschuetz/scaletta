65f387cdd08c13b72b43dce309477abad3933ca4
Kevin Schuetz <schuetzk@gmail.com>
2026-07-11 23:00:09 -0500

    LogicalOps: replace unsafeRead calls with read

--- shared/src/main/scala/software/kes/scaletta/internal/runtime/LogicalOps.scala
+++ shared/src/main/scala/software/kes/scaletta/internal/runtime/LogicalOps.scala
@@ -4,15 +4,15 @@ import software.kes.scaletta.api.ArgumentReader
 
 object LogicalOps {
   def and(args: ArgumentReader): Boolean =
-    args.unsafeReadBoolean(0) &&
+    args.readBoolean(0) &&
       args.unsafeReadThunk[Boolean](1)
         .apply()
 
   def or(args: ArgumentReader): Boolean =
-    args.unsafeReadBoolean(0) ||
+    args.readBoolean(0) ||
       args.unsafeReadThunk[Boolean](1)
         .apply()
 
   def not(args: ArgumentReader): Boolean =
-    !args.unsafeReadBoolean(0)
+    !args.readBoolean(0)
 }
