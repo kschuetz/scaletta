f27c6c8ba86e95ee6ae361a27f146a8a3a0eeaf7
Kevin Schuetz <schuetzk@gmail.com>
2026-07-15 21:53:42 -0500

    RuntimeCallTarget

---
+++ shared/src/main/scala/software/kes/scaletta/internal/interpreter/RuntimeCallTarget.scala
@@ -0,0 +1,26 @@
+package software.kes.scaletta.internal.interpreter
+
+import software.kes.scaletta.api.CallTarget
+
+/**
+ * A runtime adapter that allows interpreted function values/closures to be exposed
+ * to native higher-order functions as [[CallTarget]].
+ */
+private[interpreter] final class RuntimeCallTarget(val closure: RuntimeClosure,
+                                                   val parameterCount: Int) extends CallTarget {
+  private val _argumentValues = new Array[Any](parameterCount)
+
+  private[interpreter] def argumentValues(index: Int): Any = {
+    if (index < 0 || index >= parameterCount) {
+      throw new IndexOutOfBoundsException(s"Argument index $index is out of bounds for function with $parameterCount parameters")
+    }
+    _argumentValues(index)
+  }
+
+  override def setArgument(index: Int, value: Any): Unit = {
+    if (index < 0 || index >= parameterCount) {
+      throw new IndexOutOfBoundsException(s"Argument index $index is out of bounds for function with $parameterCount parameters")
+    }
+    _argumentValues(index) = value
+  }
+}
---
+++ shared/src/test/scala/software/kes/scaletta/internal/interpreter/RuntimeCallTargetSpec.scala
@@ -0,0 +1,75 @@
+package software.kes.scaletta.internal.interpreter
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class RuntimeCallTargetSpec extends AnyFunSuite with Matchers {
+  test("creating a runtime call target") {
+    val captureSignature = new CaptureSignature(0, 0, 0, 0, 0, 0, 0, 0, 0)
+    val capturedFrame = new CapturedFrame(captureSignature)
+    val closure = new RuntimeClosure(41, capturedFrame)
+    val target = new RuntimeCallTarget(closure, 3)
+
+    target.closure shouldBe closure
+    target.parameterCount shouldBe 3
+  }
+
+  test("setting and getting arguments") {
+    val captureSignature = new CaptureSignature(0, 0, 0, 0, 0, 0, 0, 0, 0)
+    val capturedFrame = new CapturedFrame(captureSignature)
+    val closure = new RuntimeClosure(41, capturedFrame)
+    val target = new RuntimeCallTarget(closure, 2)
+
+    target.setArgument(0, "first")
+    target.setArgument(1, 43)
+
+    target.argumentValues(0) shouldBe "first"
+    target.argumentValues(1) shouldBe 43
+  }
+
+  test("overwriting an argument") {
+    val captureSignature = new CaptureSignature(0, 0, 0, 0, 0, 0, 0, 0, 0)
+    val capturedFrame = new CapturedFrame(captureSignature)
+    val closure = new RuntimeClosure(41, capturedFrame)
+    val target = new RuntimeCallTarget(closure, 1)
+
+    target.setArgument(0, "initial")
+    target.argumentValues(0) shouldBe "initial"
+
+    target.setArgument(0, "overwritten")
+    target.argumentValues(0) shouldBe "overwritten"
+  }
+
+  test("rejecting invalid argument indexes") {
+    val captureSignature = new CaptureSignature(0, 0, 0, 0, 0, 0, 0, 0, 0)
+    val capturedFrame = new CapturedFrame(captureSignature)
+    val closure = new RuntimeClosure(41, capturedFrame)
+    val target = new RuntimeCallTarget(closure, 2)
+
+    assertThrows[IndexOutOfBoundsException] {
+      target.setArgument(-1, "invalid")
+    }
+
+    assertThrows[IndexOutOfBoundsException] {
+      target.setArgument(2, "invalid")
+    }
+
+    assertThrows[IndexOutOfBoundsException] {
+      // Using apply to test index validation if it was there, but it's an array
+      target.argumentValues(-1)
+    }
+
+    assertThrows[IndexOutOfBoundsException] {
+      target.argumentValues(2)
+    }
+  }
+
+  test("preserving the wrapped target identity") {
+    val captureSignature = new CaptureSignature(0, 0, 0, 0, 0, 0, 0, 0, 0)
+    val capturedFrame = new CapturedFrame(captureSignature)
+    val closure = new RuntimeClosure(41, capturedFrame)
+    val target = new RuntimeCallTarget(closure, 0)
+
+    target.closure shouldBe closure
+  }
+}
